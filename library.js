54a5581d0f2ea67ed6ac1645f47dc23e0f003211
Lwazi Mabota <greninja71@gmail.com>
2022-01-29 21:20:23 -0800

    Added dropdown menu with book reading status, and pushed it within library object

--- library.js
+++ library.js
@@ -1,32 +1,45 @@
+let myLibrary = [];
 const newBook = document.querySelector('.new-book');
 const bookSumbit = document.querySelector(".submit");
-document.querySelector(".bookValues").style.display = "none";
-let myLibrary = [];
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("page-count");
+const bookOptions = document.querySelector('.status');
+const openButton = document.querySelector(".openButton");
+const para1 =  document.getElementById("p1");
+const para2 = document.getElementById("p2");
+const para3 = document.getElementById("p3");
+const para4 = document.getElementById("p4");
 
-function Book(title,author,pages,isRead){
+document.querySelector(".bookValues").style.display = "none";
+
+function Book(title,author,pages,read){
      this.title = title;
      this.author = author;
      this.pages = pages;
-     this.isRead = isRead;
+     this.read = read;
      this.info = function(){
          return this.title + " written by " + this.author + " has " + this.pages + " pages " + isRead;
      }
-} 
-function addBooktoLibrary(title,author,pages,isRead) {
-    myLibrary.push(title,author,pages,isRead);
 }
-function addBook(){
+function addBooktoLibrary(){
+    if (bookTitle.value && bookAuthor.value && bookPages.value && bookOptions.value){
+        myLibrary.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookOptions.value))
+    } else{
+        alert("Please enter all information")
+    }
+    console.log(myLibrary)
+}
+openButton.addEventListener('click',() => {
     document.getElementById("bookForm").style.display = "block";
-}
+})
 function closeBook() {
     document.getElementById("bookForm").style.display = "none";
 }
 bookSumbit.addEventListener('click', () => {
-    var strText1 = document.getElementById("title").value;
-    var strText2 = document.getElementById("author").value;
-    var strText3 = document.getElementById("page-count").value;
-    document.getElementById("p1").innerHTML = strText1;
-    document.getElementById("p2").innerHTML = strText2;
-    document.getElementById("p3").innerHTML = strText3;
+    para1.innerHTML += bookTitle.value;
+    para2.innerHTML += bookAuthor.value;
+    para3.innerHTML += bookPages.value;
+    para4.innerHTML += bookOptions.value;
     document.querySelector(".bookValues").style.display = "block";
 })
