76ae417e0baf51fbc4d771ba584298de3c4b6a4d
Lwazi Mabota <greninja71@gmail.com>
2022-02-03 18:30:36 -0800

    Added Delete Event, to erase an Individual Book on user screen

--- library.js
+++ library.js
@@ -7,29 +7,35 @@ const bookPages = document.getElementById("page-count");
 const bookOptions = document.querySelector('.status');
 const openButton = document.querySelector(".openButton");
 const bookValues = document.querySelector(".bookValues");
+const bookOutline = document.querySelector(".book");
+const bookDelete = document.querySelector(".delete");
+const individualBook = document.querySelector(".book");
+const bookForm = document.getElementById("bookForm");
+bookValues.style.display = "none";
 
 var para1 = document.createElement("p");
 var para1Text = document.createTextNode("Title:");
+para1.classList.add('para-title');
 para1.appendChild(para1Text);
-bookValues.appendChild(para1);
+bookOutline.appendChild(para1);
 
 var para2 = document.createElement("p");
 var para2Text = document.createTextNode("Author:");
+para2.classList.add('para-author')
 para2.appendChild(para2Text);
-bookValues.appendChild(para2);
+bookOutline.appendChild(para2);
 
 var para3 = document.createElement("p");
 var para3Text = document.createTextNode("Pages:");
 para3.appendChild(para3Text);
-bookValues.appendChild(para3);
+para3.classList.add('para-pages')
+bookOutline.appendChild(para3);
 
 var para4= document.createElement("p");
 var para4Text = document.createTextNode("Status:");
 para4.appendChild(para4Text);
-bookValues.appendChild(para4);
+bookOutline.appendChild(para4);
 
-
-document.querySelector(".bookValues").style.display = "none";
 
 function Book(title,author,pages,read){
      this.title = title;
@@ -49,15 +55,20 @@ function addBooktoLibrary(){
     console.log(myLibrary)
 }
 openButton.addEventListener('click',() => {
-    document.getElementById("bookForm").style.display = "block";
+    bookForm.style.display = "block";
 })
 function closeBook() {
-    document.getElementById("bookForm").style.display = "none";
+    bookForm.style.display = "none";
 }
 bookSumbit.addEventListener('click', () => {
     para1.innerHTML += bookTitle.value;
     para2.innerHTML += bookAuthor.value;
     para3.innerHTML += bookPages.value;
     para4.innerHTML += bookOptions.value;
-    document.querySelector(".bookValues").style.display = "block";
+    bookValues.style.display = "block";
+})
+bookDelete.addEventListener("click", function(e){
+    e.target.parentNode.remove()
+    bookValues.style.display = "none";
+    
 })
