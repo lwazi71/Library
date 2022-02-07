973804322512e9228a822165b73eeb60632fdad9
Lwazi Mabota <greninja71@gmail.com>
2022-02-06 21:06:05 -0800

    Added function to change book's read status text based on click event

--- library.js
+++ library.js
@@ -3,14 +3,23 @@ const bookSumbit = document.querySelector(".submit");
 const bookTitle = document.getElementById("title");
 const bookAuthor = document.getElementById("author");
 const bookPages = document.getElementById("page-count");
-const bookOptions = document.querySelector('.status');
 const openButton = document.querySelector(".openButton");
 const bookValues = document.querySelector(".bookValues");
 const bookOutline = document.querySelector(".book");
 const bookDelete = document.querySelector(".delete");
 const individualBook = document.querySelector(".book");
 const bookForm = document.getElementById("bookForm");
+const bookProgress = document.querySelector(".read-progress");
+const bookOptions = document.querySelector('.status');
 
+const para1 = document.createElement("p");
+const  para1Text = document.createTextNode("Title:");
+const para2 = document.createElement("p");
+const para2Text = document.createTextNode("Author:");
+const para3 = document.createElement("p");
+const para3Text = document.createTextNode("Pages:");
+const para4 = document.createElement("p");
+const para4Text = document.createTextNode("Status:");
 window.onload = function() {
     bookValues.style.display = "none";
 }
@@ -29,53 +38,53 @@ newBook.addEventListener("click",openBook);
 
 let myLibrary = [];
 
-var para1 = document.createElement("p");
-var para1Text = document.createTextNode("Title:");
-para1.classList.add('para-title');
-para1.appendChild(para1Text);
-bookOutline.appendChild(para1);
+function bookStyling() {
+    para1.classList.add('para-title');
+    para1.appendChild(para1Text);
+    bookOutline.appendChild(para1);
 
-var para2 = document.createElement("p");
-var para2Text = document.createTextNode("Author:");
-para2.classList.add('para-author')
-para2.appendChild(para2Text);
-bookOutline.appendChild(para2);
+    para2.classList.add('para-author')
+    para2.appendChild(para2Text);
+    bookOutline.appendChild(para2);
 
-var para3 = document.createElement("p");
-var para3Text = document.createTextNode("Pages:");
-para3.appendChild(para3Text);
-para3.classList.add('para-pages')
-bookOutline.appendChild(para3);
+    para3.appendChild(para3Text);
+    para3.classList.add('para-pages')
+    bookOutline.appendChild(para3);
 
-var para4 = document.createElement("p");
-var para4Text = document.createTextNode("Status:");
-para4.appendChild(para4Text);
-bookOutline.appendChild(para4);
+    para4.appendChild(para4Text);
+    bookOutline.appendChild(para4);
 
+}
 function Book(title,author,pages,read){
      this.title = title;
      this.author = author;
      this.pages = pages;
      this.read = read;
-     this.info = function(){
-         return this.title + " written by " + this.author + " has " + this.pages + " pages " + isRead;
-     }
 }
-
 bookSumbit.addEventListener("click", addBooktoLibrary);
 
 function addBooktoLibrary(){
+    bookStyling()
     para1.innerHTML += bookTitle.value;
     para2.innerHTML += bookAuthor.value;
     para3.innerHTML += bookPages.value;
     para4.innerHTML += bookOptions.value;
-    let newBook = new Book(bookTitle.value, bookAuthor.value,bookPages.value,bookOptions.value);
+    let newBook = new Book(bookTitle.value, bookAuthor.value,bookPages.value, bookOptions.value);
     myLibrary.push(newBook);
     bookValues.style.display = "block";
     console.log(myLibrary)
 }
-bookSumbit.addEventListener("click", addBooktoLibrary);
-
+function toogleBook() {
+    var a = ["In progress", "Not Read","Read"];
+    let i = 0;
+    let interval = setInterval(() => {
+        para4.innerHTML = a[i];
+        i++;
+        if (i == 3) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
 bookDelete.addEventListener("click", function(e){
     e.target.parentNode.remove()
     bookValues.style.display = "none";
